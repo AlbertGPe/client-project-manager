3cc6d06d3d7359a3f3c1a507f4291bf8d1a40568
AlbertGPe <albertgarciapedrosa@gmail.com>
2026-03-02 13:09:37 +0100

    base app.js & connection to mongo

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,31 @@
 require('dotenv').config();
 
 const express = require('express');
+const logger = require('morgan');
+const createError = require('http-errors');
+
+require('./src/config/db.config');
+
 const app = express();
+
+app.use(logger('dev'))
+
+
+
+app.use((req, res, next) => next(createError(404, 'Route not found')))
+
+app.use((error, req, res, next) => {
+  if (!error.status) {
+    error = createError(500 , error)
+  }
+
+  const data = {
+    message: error.message
+  }
+
+  res.status(error.status)
+    .json(data)
+})
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => console.log(`Aplication running at port ${port}`))
---
+++ backend/src/config/db.config.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/client-project-manager';
+
+require("node:dns/promises").setServers(["1.1.1.1", "8.8.8.8"]);
+
+mongoose.connect(MONGODB_URI)
+  .then(() => console.info('Successfully connected to the database'))
+  .catch((error) => console.error('An error ocurred connecting to the database', error))
